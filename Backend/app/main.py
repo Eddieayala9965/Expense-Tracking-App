2dd54012c0119daa77e4693b9491bf1dbf2edfea
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-07-19 16:17:59 -0400

    added middleware for cors policy error, easy fix, already knew the issue

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import engine, Base
 from .api.endpoints import expense
+
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://localhost:5173"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(expense.router)
 
